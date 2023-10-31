60d4356b8fad575d07bd6ffc6a949009ed8a1081
tu-hm <hoanghoangminhtu1708@gmail.com>
2023-10-31 09:14:55 +0700

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,4 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   constructor() {}
-
-  async validateUser(username: string, password: string): Promise<any> {
-    if (username === 'admin' && password === 'admin') {
-      return { username: 'admin' };
-    }
-    return null;
-  }
 }
